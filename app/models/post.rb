40b365d2f8e28a32da83803f94951d6a9be9e4b4
Oguzhan Ince <oguzhan824@gmail.com>
2022-02-06 21:25:22 +0300

    comments added in Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments
 
   validates :body, presence: true
   validates :body, length: { minimum: 1, maximum: 250 }
